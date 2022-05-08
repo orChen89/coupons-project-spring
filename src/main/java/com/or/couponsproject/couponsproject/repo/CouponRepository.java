ddf27a681c2304362bdf41c9d520ca184d38b4f3
orChen89 <orooshch0@gmail.com>
2022-05-08 19:22:02 +0300

    Updated!

--- src/main/java/com/or/couponsproject/couponsproject/repo/CouponRepository.java
+++ src/main/java/com/or/couponsproject/couponsproject/repo/CouponRepository.java
@@ -12,4 +12,5 @@ public interface CouponRepository extends JpaRepository <Coupon, Long> {
     List<Coupon> getCouponsByCompanyId (Long companyId);
 
     List<Coupon> getCouponsByCustomersId (Long customerId);
+
 }
